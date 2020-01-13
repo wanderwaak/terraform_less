451127f594720621690f2d5833dc9f029b231173
Nikolay <nasidoruk@ukr.net>
2020-01-13 22:30:01 +0200

    add_tags

--- Lesson_1/Lesson_1.tf
+++ Lesson_1/Lesson_1.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = "{}"
+  secret_key = "{}"
   region     = "eu-central-1"
 }
 
@@ -8,9 +8,21 @@ resource "aws_instance" "my_Ubuntu" {
   count         = 3
   ami           = "ami-0cc0a36f626a4fdf5"
   instance_type = "t3.micro"
+
+  tags = {
+    name    = "My ubuntu server"
+    owner   = "N Sid"
+    project = "Terraform lessons"
+  }
 }
 
 resource "aws_instance" "my_AWS_linux" {
   ami           = "ami-0d4c3eabb9e72650a"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
+
+  tags = {
+    name    = "My amazon server"
+    owner   = "N Sid"
+    project = "Terraform lessons"
+  }
 }
