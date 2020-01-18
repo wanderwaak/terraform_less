eef1fd80cf7c2d47dce6142302d51cc3e74d8b9f
Nikolay <nasidoruk@ukr.net>
2020-01-18 22:17:25 +0200

    less_2

---
+++ Lesson-2/webServer.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-03a71cec707bfc3d7"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip = `curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+}
+
+
+resource "aws_security_group" "my_webserver" {
+  name        = "web_security_group"
+  description = "web_security_group"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
